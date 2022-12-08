82b4237ca843c9f8598ceddb17c814797c449002
irshad007 <irshadmuhammedch8@gmail.com>
2022-12-08 22:36:09 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Spring_Student_app_frontend';
+}
